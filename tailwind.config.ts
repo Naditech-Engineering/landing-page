ee4ceba40f638860094619d56424fcb43b5012f8
6NvckAi9 <149785092+6Nvckai9@users.noreply.github.com>
2025-03-27 15:46:09 +0800

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    
+  },
+  plugins: [],
+} satisfies Config;
+
+
+
