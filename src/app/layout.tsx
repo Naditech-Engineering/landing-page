b8c34828332b4640ec1674f97943ba7acf9acd14
6NvckAi9 <149785092+6Nvckai9@users.noreply.github.com>
2025-03-28 13:07:02 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,29 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "./components/Home/Navbar/ResponsiveNav";
 import Footer from "./components/Home/Footer/Footer";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
 const font = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Yuki Suou | ID Discord Bot",
   description: "Lets Make Discord Server More Interesting and Attractive with Yuki Suou ID",
+  openGraph: {
+    type: 'website',
+    url: 'https://yukisuou.xyz', // Replace with your actual domain
+    title: "Yuki Suou | ID Discord Bot",
+    description: "Lets Make Discord Server More Interesting and Attractive with Yuki Suou ID",
+    images: [
+      {
+        url: 'https://yukisuou.xyz/images/web_image.png', // Replace with your actual image URL
+        alt: 'Yuki Suou ID Discord Bot',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -32,9 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${font.className} antialiased`}
-      >
+      <body className={`${font.className} antialiased`}>
         <ResponsiveNav />
         {children}
         <Footer />
