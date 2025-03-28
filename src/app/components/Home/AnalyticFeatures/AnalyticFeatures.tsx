afcf80cc7369699006958766824de2ae08aa7268
6NvckAi9 <149785092+6Nvckai9@users.noreply.github.com>
2025-03-28 17:55:24 +0800

    update

--- src/app/components/Home/AnalyticFeatures/AnalyticFeatures.tsx
+++ src/app/components/Home/AnalyticFeatures/AnalyticFeatures.tsx
@@ -49,7 +49,7 @@ function AnalyticFeatures() {
           </ul>
           <Link href="https://dash.yukisuou.xyz/invite" target="_blank" rel="noopener noreferrer">
             <button className="mt-8 px-8 py-3 bg-gray-100 text-gray-800 font-semibold rounded-full hover:bg-[#F86401] transition-all duration-200 hover:text-white">
-              Invite &rarr;
+              Invite
             </button>
           </Link>
         </div>
