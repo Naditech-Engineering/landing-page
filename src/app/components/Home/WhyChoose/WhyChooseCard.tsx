bac1cbd01922d64130d1ae51b559be9441ee15df
6NvckAi9 <149785092+6Nvckai9@users.noreply.github.com>
2025-03-28 17:50:26 +0800

    update

--- src/app/components/Home/WhyChoose/WhyChooseCard.tsx
+++ src/app/components/Home/WhyChoose/WhyChooseCard.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import React from "react";
-import { FaArrowRightLong } from "react-icons/fa6";
 
 type Props = {
   title: string;
@@ -25,9 +24,8 @@ const WhyChooseCard = function({ image, title, description, linkText }: Props) {
       <p className="text-gray-600 text-center font-medium text-sm mb-7">
         {description}
       </p>
-      <p className="text-center font-semibold cursor-pointer flex items-center justify-center hover:text-[#E93306] transition-all duration-200">
+      <p className="text-center font-semibold cursor-pointer hover:text-[#E93306] transition-all duration-200">
         {linkText}
-        <FaArrowRightLong className="w-4 h-4 ml-1 hover:text-[#E93306] transition-all duration-200" />
       </p>
     </div>
   );
