270e50f74a155c7f610888e2b0746f5d30d3a634
6NvckAi9 <149785092+6Nvckai9@users.noreply.github.com>
2025-03-28 10:45:58 +0800

    update

--- src/app/components/Home/Price/PriceCard.tsx
+++ src/app/components/Home/Price/PriceCard.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaCheck } from "react-icons/fa6";
 
-type priceProps = { price: number; plan: string; };
+type priceProps = {
+  price: number;
+  plan: string;
+};
 
 function PriceCard({ price, plan }: priceProps) {
   const priceFeatures = [
@@ -13,19 +16,16 @@ function PriceCard({ price, plan }: priceProps) {
     { feature: "Exclusive Discounts" },
   ];
 
-  const handleBuyClick = () => {
-    // Load the script dynamically
-    const script = document.createElement("script");
-    script.src = "https://storage.sociabuzz.com/storage/js/main/buttononwebsite/index.min.js";
-    script.async = true;
-    script.onload = () => {
-      // Call the function to display the purchase interface
-      if (window.sbBoW) {
-        window.sbBoW.draw("yukisuou", "QlVZ", "position-top-middle", "#ff8600", "#FFFFFF");
-      }
-    };
-    document.body.appendChild(script);
-  };
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.type = "text/javascript";
+    script1.src = "https://storage.sociabuzz.com/storage/js/main/buttononwebsite/index.min.js";
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = 'sbBoW.draw("yukisuou","QlVZ","position-top-middle","#ff8600","#FFFFFF")';
+    document.body.appendChild(script2);
+  }, []);
 
   return (
     <div className="bg-white p-10 rounded-lg shadow-lg">
@@ -38,7 +38,10 @@ function PriceCard({ price, plan }: priceProps) {
       <p className="mt-2 text-gray-600 text-center">Per server</p>
       <div className="mt-10">
         {priceFeatures.map((item, index) => (
-          <div key={index} className="text-center mb-4 flex items-center space-x-3">
+          <div
+            key={index}
+            className="text-center mb-4 flex items-center space-x-3"
+          >
             <div className="text-center w-8 h-8 bg-gray-300 rounded-full justify-center flex items-center flex-col">
               <FaCheck className="text-green-500" />
             </div>
@@ -49,12 +52,9 @@ function PriceCard({ price, plan }: priceProps) {
         ))}
       </div>
       <div className="mt-8">
-        <button
-          onClick={handleBuyClick}
-          className="block w-full p-3 text-base md:text-lg text-white font-bold bg-gradient-to-r from-[#F86401] via-[#E93306] via-[#F86401] to-[#FFA500] hover:from-[#FF5733] hover:via-[#FFA500] hover:to-[#FFD700] transition-all duration-300"
-        >
+        <div id="sociabuzz-button" className="block w-full p-3 text-base md:text-lg text-white font-bold bg-gradient-to-r from-[#F86401] via-[#E93306] via-[#F86401] to-[#FFA500] hover:from-[#FF5733] hover:via-[#FFA500] hover:to-[#FFD700] transition-all duration-300">
           BUY
-        </button>
+        </div>
       </div>
     </div>
   );
