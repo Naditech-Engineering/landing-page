2437242a6200d6f879ad320eec0ce8c3b3bf8b08
6NvckAi9 <149785092+6Nvckai9@users.noreply.github.com>
2025-04-20 13:02:50 +0800

    Update page.tsx

--- src/app/popular/3/page.tsx
+++ src/app/popular/3/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PrivacyPolicy: React.FC = () => {
   return (
-    <div className="bg-white text-black container mx-auto p-8">
+    <div className="bg-white text-black container mx-auto pt-24 p-8">
       <h1 className="text-3xl font-bold mb-6">Privacy Policy of Yuki Suou</h1>
       <p className="text-lg mb-4">This Application collects some Personal Data from its Users.</p>
 
