066358888cb65bd45f5c314e8875aa86c7c65dea
6NvckAi9 <149785092+6Nvckai9@users.noreply.github.com>
2025-05-01 10:38:43 +0800

    update

--- src/app/components/Home/MostPopular/MostPopularCard.tsx
+++ src/app/components/Home/MostPopular/MostPopularCard.tsx
@@ -11,26 +11,24 @@ type ImgProps = {
 
 const MostPopularCard = ({ id, title, image, description }: ImgProps) => {
   return (
-    <Link href={/popular/${id}}>
-       <div className="flex justify-center items-center">
-      <div className="bg-gradient-to-r from-[#FD8E3F] [background-size:200%_100%] via-[#F8D3A9] to-[#F8D3A9] relative w-80 h-96 rounded-xl shadow-xl overflow-hidden">
-        {/* Full-size image */}
-        <Image src={image} alt={title} layout="fill" objectFit="contain" />
-        {/* Overlay for title and description */}
-        <div
-          className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black via-transparent to-transparent p-5"
-          style={{
-            background:
-              "linear-gradient(to top, #000000 50%, transparent 100%)",
-          }}
-        >
-          <h1 className="text-white text-xl font-semibold">{title}</h1>
-          <p className="text-white text-sm mt-2">{description}</p>
+    <Link href={`/popular/${id}`}>
+      <div className="flex justify-center items-center">
+        <div className="bg-gradient-to-r from-[#FD8E3F] [background-size:200%_100%] via-[#F8D3A9] to-[#F8D3A9] relative w-80 h-96 rounded-xl shadow-xl overflow-hidden bg-white">
+          {/* Full-size image */}
+          <Image src={image} alt={title} layout="fill" objectFit="contain" />
+          {/* Overlay for title and description */}
+          <div
+            className="absolute bottom-0 left-0 w-full p-5"
+            style={{
+              backgroundColor: "white",
+            }}
+          >
+            <h1 className="text-black text-xl font-semibold">{title}</h1>
+            <p className="text-black text-sm mt-2">{description}</p>
+          </div>
         </div>
       </div>
-    </div>
     </Link>
-   
   );
 };
 
